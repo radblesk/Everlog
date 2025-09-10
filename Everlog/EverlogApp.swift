514360da443b5375ea8502f5e7731f9838db2631
radblesk <144258807+radblesk@users.noreply.github.com>
2025-09-10 20:00:53 +0200

    Initial Commit

---
+++ Everlog/EverlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  EverlogApp.swift
+//  Everlog
+//
+//  Created by Radoslav Bley on 10/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct EverlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
