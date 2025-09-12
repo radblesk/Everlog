cd260a83be3469731771d708dece5da56471e571
radblesk <144258807+radblesk@users.noreply.github.com>
2025-09-12 04:18:22 +0200

    feat: added modelContainer to main app

--- Everlog/EverlogApp.swift
+++ Everlog/EverlogApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct EverlogApp: App {
@@ -13,5 +14,6 @@ struct EverlogApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [Mac.self])
     }
 }
